9ebe5790de75bd9eeff17812894ca38a914ef79e
zk <1819592017@qq.com>
2022-05-07 10:16:14 +0800

    条件构造器和常用接口

--- mybatisPlusDemo/src/test/java/com/my/MybatisWrapperTest.java
+++ mybatisPlusDemo/src/test/java/com/my/MybatisWrapperTest.java
@@ -1,9 +1,12 @@
 package com.my;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.my.entity.User;
 import com.my.mapper.UserMapper;
+import net.minidev.json.writer.UpdaterMapper;
 import org.junit.Test;
+import org.junit.platform.commons.util.StringUtils;
 import org.junit.runner.RunWith;
 import org.junit.runner.Runner;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +19,7 @@ import java.util.List;
 @SpringBootTest
 public class MybatisWrapperTest {
 
-    @Autowired
+    @Autowired(required = false)
     private UserMapper userMapper;
 
     //测试组合查询条件
@@ -69,5 +72,62 @@ public class MybatisWrapperTest {
         QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
         userQueryWrapper.select("name","age");
         List<User> users = userMapper.selectList(userQueryWrapper);
+    }
+
+    @Test
+    //实现子查询
+    public void test06(){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.inSql("id","select id from t_user where id<=3");
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    //UpdateWrapper
+    public void test07(){
+        UpdateWrapper<User> userUpdateWrapper = new UpdateWrapper<>();
+        userUpdateWrapper.set("name","Time").set("age",20).set("email","1819592017@qq.com")
+                .like("name","a").and(i -> i.gt("age",20).isNotNull("email"));
+        int update = userMapper.update(null, userUpdateWrapper);
+        System.out.println("受影响的行数为："  + update);
+
+    }
+
+    @Test
+    //condition
+    public void test08(){
+
+        String username = null;
+        Integer ageBegin = 10;
+        Integer ageEnd = 10;
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like(StringUtils.isNotBlank(username),"username",username).gt(ageBegin != null,"age",ageBegin)
+                .le(ageEnd != null ,"age",ageEnd);
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    //LambdaQueryWrapper
+    public void test09(){
+        String username = null;
+        Integer ageBegin = 10;
+        Integer ageEnd = 10;
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda().like(StringUtils.isNotBlank(username),User::getName,username).gt(ageBegin != null,User::getAge,ageBegin)
+                .le(ageEnd != null ,User::getAge,ageEnd);
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    //LambdaUpdateWrapper
+    public void test10(){
+        UpdateWrapper<User> userUpdateWrapper = new UpdateWrapper<>();
+        userUpdateWrapper.lambda().set(User::getName,"Times").set(User::getAge,23).set(User::getEmail,"1819592017@qq.com")
+                .like(User::getName,"a").and(i -> i.gt(User::getAge,20).isNotNull(User::getEmail));
+        int update = userMapper.update(null, userUpdateWrapper);
+        System.out.println("受影响的行数为："  + update);
     }
 }
